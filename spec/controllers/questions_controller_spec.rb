165b90c326cb8c31fb73a6e049338a7228ad2000
Alexei Shilin <alexei.shilin@gmail.com>
2014-09-22 16:23:15 +0700

    adds and passes spec for new @question for new template

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe QuestionsController, :type => :controller do
   describe 'GET #new' do
     before { get :new }
     it 'creates a new question and assigns it to @question' do
-      
+      expect(assigns(:question)).to be_a_new(Question)
     end
     it 'renders a new view' do
       expect(response).to render_template :new
