4c92bed4892dd3fc0aa212355d276827d98dc420
Alexei Shilin <alexei.shilin@gmail.com>
2014-09-30 01:22:02 +0700

    Users can answer

---
+++ spec/feature/create_answers_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature 'User answers question', %q{
+In order to share my knowledge
+As an authenticated user
+I want to be able to create answers
+} do
+
+  given(:user) {create(:user)}
+  given(:question) {create(:question)}
+  scenario 'Authenticated user creates an answer' do
+    sign_in(user)
+    visit question_path(question)
+    fill_in  'Your answer', with: 'MyAnswer'
+    click_on 'Create'
+
+    expect(current_path).to eq question_path(question)
+    within '.answers' do
+      expect(page).to have_content 'MyAnswer'
+    end
+  end
+
+  
+end
