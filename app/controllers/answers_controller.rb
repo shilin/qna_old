7c00f8aea46f38fae5851d18a34b782fdf3b7827
Alexei Shilin <alexei.shilin@gmail.com>
2014-09-30 18:33:52 +0700

    Refactored request as js format

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,7 +3,7 @@ class AnswersController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
     @question.answers.create(answer_params)
-      render 'create.js.erb'
+      #render 'create.js.erb'
     
   end
 
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe AnswersController, :type => :controller do
 
     context 'with valid attributes' do
       it 'saves a new answer to the database' do
-        expect { post :create, question_id: question.id, answer: attributes_for(:answer) }.to change(question.answers, :count).by(1)
+        expect { post :create, question_id: question.id, answer: attributes_for(:answer), format: :js }.to change(question.answers, :count).by(1)
       end
 
     #  it 'stays on question page' do
@@ -19,7 +19,7 @@ RSpec.describe AnswersController, :type => :controller do
 
     context 'with invalid attributes' do
       it 'fails to save an answer' do
-        expect { post :create, answer: attributes_for(:invalid_answer), question_id: question }.to_not change(Answer, :count)
+        expect { post :create, answer: attributes_for(:invalid_answer), question_id: question, format: :js }.to_not change(Answer, :count)
       end
     #  it 'redirects to question show view' do
     #    post :create, answer:  attributes_for(:invalid_answer), question_id: question
