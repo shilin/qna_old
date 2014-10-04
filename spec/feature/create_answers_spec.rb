47b22f9792e7b4aa287f9a992320536733b288fb
Alexei Shilin <alexei.shilin@gmail.com>
2014-10-04 13:45:54 +0700

    refactored create answer spec a bit

--- spec/feature/create_answers_spec.rb
+++ spec/feature/create_answers_spec.rb
@@ -9,24 +9,33 @@ I want to be able to create answers
   given(:user) {create(:user)}
   given(:question) {create(:question)}
 
-  scenario 'Authenticated user creates an answer', js: true do
-    sign_in(user)
-    visit question_path(question)
-    fill_in  'Your answer', with: 'MyAnswer'
-    click_on 'Create'
+  describe 'Authenticated user' do
 
-    expect(current_path).to eq question_path(question)
-    within '.answers' do
-      expect(page).to have_content 'MyAnswer'
+    before do
+      sign_in(user)
+    end
+
+    scenario 'creates an answer', js: true do
+      visit question_path(question)
+      fill_in  'Your answer', with: 'MyAnswer'
+      click_on 'Create'
+      expect(current_path).to eq question_path(question)
+      within '.answers' do
+        expect(page).to have_content 'MyAnswer'
+      end
+    end
+
+    scenario 'User tries to create invalid answer', js: true do
+      visit question_path(question)
+      click_on 'Create'
+      expect(page).to have_content "Body can't be blank"
     end
   end
 
-  scenario 'User tries to create invalid answer', js: true do
-    sign_in user
-    visit question_path(question)
-    click_on 'Create'
+  describe 'Unauthenticated user' do
 
-    expect(page).to have_content "Body can't be blank"
+    scenario 'tries to create an answer'
+
   end
-  
+
 end
