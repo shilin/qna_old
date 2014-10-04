c299a1ad16e2b07308a7002bc394d00569a99475
Alexei Shilin <alexei.shilin@gmail.com>
2014-10-04 16:00:29 +0700

    Authenticated user only can create question

--- spec/feature/question_spec.rb
+++ spec/feature/question_spec.rb
@@ -5,20 +5,35 @@ In order to get the answers
 As a user
 I want to be able to ask questions
 } do
-  scenario 'User tries to submit a valid question' do
-    visit new_question_path
-    fill_in 'Title', with: 'Sample Title'
-    fill_in 'Text', with: 'Sample Body'
-    click_on 'Create'
-    expect(page).to have_content 'Вопрос успешно задан'
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  describe 'Authenticated user' do
+    before do
+      sign_in(user)
+      visit new_question_path
+    end
+
+    scenario 'tries to submit a valid question' do
+      fill_in 'Title', with: question.title
+      fill_in 'Text', with: question.body
+      click_on 'Create'
+      expect(page).to have_content 'Вопрос успешно задан'
+    end
+
+    scenario 'tries to submit a invalid question' do
+      fill_in 'Title', with: question.title
+      click_on 'Create'
+      expect(current_path).to eq new_question_path
+    end
   end
 
-  scenario 'User tries to submit a invalid question' do
-    visit new_question_path
-    fill_in 'Title', with: 'Sample Title'
-    #fill_in 'Text', with: 'Sample Body'
-    click_on 'Create'
+  describe 'Unauthenticated user' do
 
-    expect(current_path).to eq new_question_path
+    scenario 'tries to submit a question' do
+      visit new_question_path
+      expect(page).to have_content 'You need to sign in or sign up before continuing'
+    end
   end
 end
