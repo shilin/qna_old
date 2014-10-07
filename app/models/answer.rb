5e48af8d583c5e1c97e0e9e3237d6f75723e0713
Alexei Shilin <alexei.shilin@gmail.com>
2014-10-08 00:25:36 +0700

    Polymorphic attachments. Answers associated with attachments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
+  has_many :attachments, as: :attachable
+  accepts_nested_attributes_for :attachments
   validates :body, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007150125) do
+ActiveRecord::Schema.define(version: 20141007171550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,10 +29,12 @@ ActiveRecord::Schema.define(version: 20141007150125) do
     t.string   "file"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "question_id"
+    t.integer  "attachable_id"
+    t.string   "attachable_type"
   end
 
-  add_index "attachments", ["question_id"], name: "index_attachments_on_question_id", using: :btree
+  add_index "attachments", ["attachable_id"], name: "index_attachments_on_attachable_id", using: :btree
+  add_index "attachments", ["attachable_type"], name: "index_attachments_on_attachable_type", using: :btree
 
   create_table "questions", force: true do |t|
     t.string   "title"
